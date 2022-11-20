bc4240d75f441bcb52932a21527d5f8ce1594142
Martin Berg Alstad <600878@stud.hvl.no>
2022-11-20 14:57:21 +0100

    Added comment about lang

--- src/main/java/com/github/martials/SimplifyTruthsRestApiApplication.java
+++ src/main/java/com/github/martials/SimplifyTruthsRestApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplifyTruthsRestApiApplication {
 
-    public static Language lang = Language.norwegianBokmaal;
+    public static Language lang = Language.norwegianBokmaal; // TODO do not use public variable, pass as reference
 
     public static void main(String[] args) {
         SpringApplication.run(SimplifyTruthsRestApiApplication.class, args);
