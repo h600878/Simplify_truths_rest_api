981578d0179f3514559a6d209360af81935eef70
Martin Berg Alstad <600878@stud.hvl.no>
2022-11-20 20:24:46 +0100

    Removed unused statuses

--- src/main/java/com/github/martials/Status.java
+++ src/main/java/com/github/martials/Status.java
@@ -1,18 +1,13 @@
 package com.github.martials;
 
-import com.github.martials.enums.Language;
 import org.jetbrains.annotations.NotNull;
 
 public record Status(int code, @NotNull String message) {
 
-    public static final Status OK, ILLEGAL_CHARACTER, MISSING_CHARACTER, NOT_FOUND;
+    public static final Status OK, NOT_FOUND;
 
     static {
-        boolean isEnglish = SimplifyTruthsRestApiApplication.lang == Language.english;
-
         OK = new Status(200, "Ok");
-        ILLEGAL_CHARACTER = new Status(501, isEnglish ? "Illegal character" : "Ugyldig tegn");
-        MISSING_CHARACTER = new Status(502, isEnglish ? "Missing character" : "Manglende tegn");
         NOT_FOUND = new Status(404, "Expression not found");
     }
 }
