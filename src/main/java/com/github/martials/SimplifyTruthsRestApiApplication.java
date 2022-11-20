ff007164a58d2e9754efb90e4d22d52da763a703
Martin Berg Alstad <600878@stud.hvl.no>
2022-11-20 20:24:29 +0100

    Removed language as a public variable

--- src/main/java/com/github/martials/SimplifyTruthsRestApiApplication.java
+++ src/main/java/com/github/martials/SimplifyTruthsRestApiApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplifyTruthsRestApiApplication {
 
-    public static Language lang = Language.norwegianBokmaal; // TODO do not use public variable, pass as reference
-
     public static void main(String[] args) {
         SpringApplication.run(SimplifyTruthsRestApiApplication.class, args);
     }
