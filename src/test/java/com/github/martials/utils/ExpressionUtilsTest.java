49043868ac9efa626653bd13d1dc5dfd2c2f7b3b
Martin Berg Alstad <600878@stud.hvl.no>
2023-01-01 14:54:51 +0100

    Refactored tests to use parameterizedTest

--- src/test/java/com/github/martials/utils/ExpressionUtilsTest.java
+++ src/test/java/com/github/martials/utils/ExpressionUtilsTest.java
@@ -2,9 +2,12 @@ package com.github.martials.utils;
 
 import com.github.martials.exceptions.IllegalCharacterException;
 import com.github.martials.exceptions.MissingCharaterException;
+import com.github.martials.exceptions.TooBigExpressionException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ExpressionUtilsTest {
 
@@ -35,49 +38,31 @@ public class ExpressionUtilsTest {
 
     }
 
-    @Test
-    void isLegalExpressionTest() {
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "[Hello]", "Å", "[Hello]⋀[World]"})
+    void isLegalExpressionTest(String value) {
+        eu.setExpression(value);
+        Assertions.assertDoesNotThrow(eu::isLegalExpression);
+    }
 
-        eu.setExpression("A");
-        Assertions.assertDoesNotThrow(eu::isLegalExpression);
-        eu.setExpression("[Hello]");
-        Assertions.assertDoesNotThrow(eu::isLegalExpression);
-        eu.setExpression("Å");
-        Assertions.assertDoesNotThrow(eu::isLegalExpression);
-        eu.setExpression("[Hello]⋀[World]");
-        Assertions.assertDoesNotThrow(eu::isLegalExpression);
+    @ParameterizedTest
+    @ValueSource(strings = {"#", "⋁", "AB", "A⋁⋀", "A(", "A[", "A⋀(B]", "A⋀()", "A¬B", "(A⋀B)(B⋀C)", "[Hello][World]"})
+    void throwsIllegalCharExpressionTest(String value) {
+        eu.setExpression(value);
+        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", "[A", "(A"})
+    void throwsMissingCharExpressionTest(String value) {
+        eu.setExpression(value);
+        Assertions.assertThrows(MissingCharaterException.class, eu::isLegalExpression);
     }
 
     @Test
-    void isIllegalExpressionTest() {
-
-        eu.setExpression("");
-        Assertions.assertThrows(MissingCharaterException.class, eu::isLegalExpression);
-        eu.setExpression("#");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("⋁");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("AB");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("A⋁⋀");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("A(");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("A[");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("[A");
-        Assertions.assertThrows(MissingCharaterException.class, eu::isLegalExpression);
-        eu.setExpression("A⋀(B]");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("A⋀()");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("A¬B");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("(A⋀B)(B⋀C)");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
-        eu.setExpression("[Hello][World]");
-        Assertions.assertThrows(IllegalCharacterException.class, eu::isLegalExpression);
+    void throwsTooBigExpressionTest() {
+        eu.setExpression("A⋀B⋀C⋀D⋀E⋀F⋀G⋀H⋀I⋀J⋀K");
+        Assertions.assertThrows(TooBigExpressionException.class, eu::isLegalExpression);
     }
 
 }
